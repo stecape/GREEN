ac524f0d752526af1f2834bc59215bbb9d3e90a6
stecape <ste.cape@gmail.com>
2022-11-28 19:38:50 +0100

    cleaned code

--- server/src/App/API/Vars/vars_api.js
+++ server/src/App/API/Vars/vars_api.js
@@ -18,17 +18,14 @@ module.exports = function (app, pool) {
   //Insert a Var
   app.post('/api/addVar', (req, res) => {
     var queryString="INSERT INTO \"" + req.body.table + "\" (\"id\",\"" + req.body.fields.join('","') + "\") VALUES (DEFAULT,'" + req.body.values.join("','") + "')"
-    console.log (queryString)
     pool.query({
       text: queryString,
       rowMode: 'array'
     })
     .then((result) => {
-      //console.log("setTag result: ", result.rows)
       res.status(201).json(result.rows)
     })
     .catch((error) => {
-      //console.log("setTag error: ", error)
       res.status(400).send(error)
     });
   });
@@ -38,9 +35,13 @@ module.exports = function (app, pool) {
     var queryString="TRUNCATE \"" + req.body.table + "\" CASCADE";
     pool.query({
       text: queryString
-    }).then((data)=>{
+    })
+    .then((data)=>{
       res.status(200)
     })
+    .catch((error) => {
+      res.status(400).send(error)
+    });
   });
 
   //Delete a Var
@@ -49,31 +50,29 @@ module.exports = function (app, pool) {
     pool.query({
       text: queryString,
       rowMode: 'array'
-    }).then((data)=>{
+    })
+    .then((data)=>{
       res.status(200).json({value: data.rows})
     })
+    .catch((error) => {
+      res.status(400).send(error)
+    });
   });
 
   //Modify a Var
   app.post('/api/updateVar', (req, res) => {
-    console.log(req.body)
     var sets = req.body.fields.map((i, index) => {
        return "\"" + i + "\" = '" + req.body.values[index] + "'"
-    })
-    console.log(sets)
-    
+    })    
     var queryString="UPDATE \"" + req.body.table + "\" SET " + sets + " WHERE id = " + req.body.id
-    console.log (queryString)
     pool.query({
       text: queryString,
       rowMode: 'array'
     })
     .then((result) => {
-      //console.log("setTag result: ", result.rows)
       res.status(201).json(result.rows)
     })
     .catch((error) => {
-      //console.log("setTag error: ", error)
       res.status(400).send(error)
     });
   });
