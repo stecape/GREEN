858ad83cd9af952cc12e1185bab1186909c45c15
Stefano Capettini <ste.cape@gmail.com>
2024-11-20 01:17:38 +0100

    generazione variabili per controllore

--- client/src/sections/Home/Home.jsx
+++ client/src/sections/Home/Home.jsx
@@ -16,13 +16,12 @@ const IsBaseType = (x, data, types)=> {
   let sentence = {}
   sentence.type = data.find(d => d.id === x).type
   sentence.result = types.find(t => t.id === sentence.type).base_type
-  console.log ("sentence: ", sentence)
   return sentence
 }
 
 export default function NoPage() {
   const ctx = useContext(ctxData)
-  let structs = { types: [], vars: [] }
+  let structs = { types: [], vars: [], tagType: [], tagId: [], tagPointer: [] }
 
   //Generazione dei tipi di dati
   //Itera l'array dei tipi andando a cercare quelli complessi, e ritorna un array di oggetti contenenti nome del type e campi.
@@ -37,25 +36,34 @@ export default function NoPage() {
 
   //Generazione delle istanze a partire dalla tabella delle Vars
   structs.vars = ctx.vars.map(v => {
-    console.log(v)
     return { id: v.id, name: v.name, type: basetypes[ctx.types.find(t => t.id === v.type).name] !== undefined ? basetypes[ctx.types.find(t => t.id === v.type).name] : ctx.types.find(t => t.id === v.type).name }
   })
 
+  
+  structs.vars.forEach(v => {
+    //le tag da inizializzare sono quelle la cui var è un tipo base oppure quelle il cui field type un tipo base (tagIsBaseType(t, ctx))
+    let initTags = ctx.tags.filter(t => (t.var === v.id && t.type_field !== null && IsBaseType(t.type_field, ctx.fields, ctx.types).result) || (t.var === v.id && IsBaseType(v.id, ctx.vars, ctx.types).result))
+
+    initTags.forEach(t => {
+      let type = t.type_field !== null ? IsBaseType(t.type_field, ctx.fields, ctx.types).type : IsBaseType(v.id, ctx.vars, ctx.types).type
+      structs.tagType.push(ctx.types.find(t => t.id === type).name.toUpperCase())
+      structs.tagId.push(t.id)
+      structs.tagPointer.push(`(void*)&${t.name}`)
+    })
+  })
   return (
     <>
       <Grid>
         <GridCell colSpan={6} className={gridStyles.item}>
           <TextContainer style={{marginLeft: '1em'}}>
             <pre>
+              {'\n#define REAL 1'}
+              {'\n#define INT 3'}
+              {'\n#define BOOL 4'}
+              {'\n#define STRING 5'}
+              {'\n#define TIMESTAMP 6\n'}
               {
-                structs.types.map(t => {
-                  let str = `
-typedef struct {${t.fields.map(f => { return ("\n\t" + f.type + " " + f.name) })}
-} ${t.name};\n`
-                  return (
-                    str
-                  )
-                })
+                structs.types.map(t => `\ntypedef struct {${t.fields.map(f => { return ("\n\t" + f.type + " " + f.name) })}\n} ${t.name};\n`)
               }
             </pre>
           </TextContainer>
@@ -64,36 +72,52 @@ typedef struct {${t.fields.map(f => { return ("\n\t" + f.type + " " + f.name) })
           <TextContainer style={{marginLeft: '1em'}}>
             <pre>
             {
-              structs.init = structs.vars.map(v => {
+              structs.vars.map(v => {
                 //le tag da inizializzare sono quelle la cui var è un tipo base oppure quelle il cui field type un tipo base (tagIsBaseType(t, ctx))
                 let initTags = ctx.tags.filter(t => (t.var === v.id && t.type_field !== null && IsBaseType(t.type_field, ctx.fields, ctx.types).result) || (t.var === v.id && IsBaseType(v.id, ctx.vars, ctx.types).result))
 
                 let inits = initTags.map(t => {
                   let type = t.type_field !== null ? IsBaseType(t.type_field, ctx.fields, ctx.types).type : IsBaseType(v.id, ctx.vars, ctx.types).type
+
                   switch(ctx.types.find(t => t.id === type).name){
                     case 'Real':
-                      console.log("Real", t.name + " = 0;")
-                      return t.name + " = 0;"
+                      return `${t.name} = 0;`
                     case 'Int':
-                      console.log("Int", t.name + " = 0;")
-                      return t.name + " = 0;"
+                      return `${t.name} = 0;`
                     case 'TimeStamp':
-                      console.log("TimeStamp", t.name + " = 0;")
-                      return t.name + " = 0;"
+                      return `${t.name} = 0;`
                     case 'Bool':
-                      console.log("Bool", t.name + " = false;")
-                      return t.name + " = false;"
+                      return `${t.name} = false;`
                     case 'String':
-                      console.log("String", t.name + " = '';")
-                      return t.name + " = '';"
+                      return `${t.name} = '';`
                     default:
-                      return ""
+                      return ''
                   }
                 })
-                console.log("inits: ", inits)
-                return ("\n" + v.type + " " + v.name + ";\n" + inits.map(e => e + "\n").join(""))
+                return (`\n${v.type} ${v.name};\n${inits.map(e => `${e} \n`).join("")}`)
               })
             }
+            </pre>
+          </TextContainer>
+        </GridCell>
+        <GridCell colSpan={4} className={gridStyles.item}>
+          <TextContainer style={{marginLeft: '1em'}}>
+            <pre>
+              {`int id[${structs.tagId.length}] = {\n\t${structs.tagId.join(`,\n\t`)}\n};`}
+            </pre>
+          </TextContainer>
+        </GridCell>
+        <GridCell colSpan={4} className={gridStyles.item}>
+          <TextContainer style={{marginLeft: '1em'}}>
+            <pre>
+              {`int type[${structs.tagType.length}] = {\n\t${structs.tagType.join(`,\n\t`)}\n};`}
+            </pre>
+          </TextContainer>
+        </GridCell>
+        <GridCell colSpan={4} className={gridStyles.item}>
+          <TextContainer style={{marginLeft: '1em'}}>
+            <pre>
+              {`void *pointer[${structs.tagPointer.length}] = {\n\t${structs.tagPointer.join(`,\n\t`)}\n};`}
             </pre>
           </TextContainer>
         </GridCell>
